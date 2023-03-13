9986a1cb9b1db5fb87cab09c783a45e29995e59f
minyu <minyu7374@gmail.com>
2023-03-13 17:16:07 +0800

    更新 sync

--- sync.sh
+++ sync.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-#########################################################################
-# File Name: ./sync.sh
-# Author: minyu
-# mail: minyu7374@gmail.com
-# Created Time: Wed 01 Apr 2020 09:00:10 PM CST
-#########################################################################
 #crontab
 # 10 2 * * * /blog-hexo/sync.sh
 
@@ -12,8 +5,10 @@ pro_dir="$(dirname "$0")"
 pre_commit_file="${pro_dir}/.sync_commit.log"
 
 commot="$(git show --stat | head -n 1 | awk '{print $NF}')"
-if [ "$commot" != "$(cat "${pre_commit_file}" 2>-)"  ]; then
+if [ "$commot" != "$(cat "${pre_commit_file}" 2>&-)"  ]; then
     hexo generate
-    rsync -avz --delete public/ root@wminyu.top:/var/www/blog
+    rsync -e 'ssh -i ~/.ssh/vps/id_rsa' -avz --delete public/ root@wminyu.top:/var/www/blog/
+    rsync -e 'ssh -i ~/.ssh/vps/id_rsa' -avz --delete public/ opc@x86.wminyu.top:/var/www/blog/
+    rsync -e 'ssh -i ~/.ssh/vps/id_rsa' -avz --delete public/ opc@arm.wminyu.top:/var/www/blog/
     echo "$commot" > "${pre_commit_file}"
 fi
